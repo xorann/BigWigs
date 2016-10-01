a58d6cffe640ade7f1a4aee33f7eae6ecb654a2b
xorann <xorann@gmail.com>
2016-10-01 18:30:03 +0200

    added experimental bars for next wave to rajaxx encounter

--- Raids/AQ20/GeneralRajaxx.lua
+++ Raids/AQ20/GeneralRajaxx.lua
@@ -18,17 +18,20 @@ L:RegisterTranslations("enUS", function() return {
 	wave_name = "Wave Alert",
 	wave_desc = "Warn for incoming waves",
 
+	trigger0 = "Remember, Rajaxx, when I said I'd kill you last?",
 	trigger1 = "Kill first, ask questions later... Incoming!",
 	trigger2 = "?????",  -- There is no callout for wave 2 ><
 	trigger3 = "The time of our retribution is at hand! Let darkness reign in the hearts of our enemies!",
-	trigger4 = "No longer will we wait behind barred doors and walls of stone! No longer will our vengeance be denied! The dragons themselves will tremble before our wrath!\013\n",
+	trigger4 = "No longer will we wait behind barred doors and walls of stone! No longer will our vengeance be denied! The dragons themselves will tremble before our wrath!",
 	trigger5 = "Fear is for the enemy! Fear and death!",
-	trigger6 = "Staghelm will whimper and beg for his life, just as his whelp of a son did! One thousand years of injustice will end this day!\013\n",
-	trigger7 = "Fandral! Your time has come! Go and hide in the Emerald Dream and pray we never find you!\013\n",
+	trigger6 = "Staghelm will whimper and beg for his life, just as his whelp of a son did! One thousand years of injustice will end this day!",
+	trigger7 = "Fandral! Your time has come! Go and hide in the Emerald Dream and pray we never find you!",
 	trigger8 = "Impudent fool! I will kill you myself!",
-	trigger9 = "Remember, Rajaxx, when I said I'd kill you last?",
 	trigger10 = "I lied...",
-
+    
+    trigger2_2 = "Kill ",
+            
+    warn0 = "Wave 1/8", -- trigger for starting the event by pulling the first wave instead of talking to andorov
 	warn1 = "Wave 1/8",
 	warn2 = "Wave 2/8",
 	warn3 = "Wave 3/8",
@@ -37,7 +40,7 @@ L:RegisterTranslations("enUS", function() return {
 	warn6 = "Wave 6/8",
 	warn7 = "Wave 7/8",
 	warn8 = "Incoming General Rajaxx",
-	warn9 = "Wave 1/8", -- trigger for starting the event by pulling the first wave instead of talking to andorov
+	
 
 } end )
 
@@ -46,6 +49,7 @@ L:RegisterTranslations("deDE", function() return {
 	wave_name = "Wellen",
 	wave_desc = "Warnung vor den ankommenden Gegner Wellen.",
 
+    trigger0 = "Erinnerst du dich daran, Rajaxx, wann ich dir das letzte Mal sagte, ich w\195\188rde dich t\195\182ten?",
 	trigger1 = "Hier kommen sie. Bleibt am Leben, Welpen.",
 	trigger2 = "?????",  -- There is no callout for wave 2 ><
 	trigger3 = "Die Zeit der Vergeltung ist gekommen!",
@@ -54,8 +58,8 @@ L:RegisterTranslations("deDE", function() return {
 	trigger6 = "Staghelm wird winseln und um sein Leben betteln, genau wie sein r\195\164udiger Sohn!",
 	trigger7 = "Fandral! Deine Zeit ist gekommen!",
 	trigger8 = "Unversch\195\164mter Narr! Ich werde Euch h\195\182chstpers\195\182nlich t\195\182ten!",
-	trigger9 = "Erinnerst du dich daran, Rajaxx, wann ich dir das letzte Mal sagte, ich w\195\188rde dich t\195\182ten?",
-
+	
+    warn0 = "Welle 1/8", -- trigger for starting the event by pulling the first wave instead of talking to andorov
 	warn1 = "Welle 1/8",
 	warn2 = "Welle 2/8",
 	warn3 = "Welle 3/8",
@@ -64,7 +68,6 @@ L:RegisterTranslations("deDE", function() return {
 	warn6 = "Welle 6/8",
 	warn7 = "Welle 7/8",
 	warn8 = "General Rajaxx kommt!",
-	warn9 = "Welle 1/8", -- trigger for starting the event by pulling the first wave instead of talking to andorov
 } end )
 
 
@@ -74,29 +77,37 @@ L:RegisterTranslations("deDE", function() return {
 ---------------------------------
 
 -- module variables
-module.revision = 20004 -- To be overridden by the module!
+module.revision = 20006 -- To be overridden by the module!
 module.enabletrigger = {module.translatedName, andorov} -- string or table {boss, add1, add2}
 module.toggleoptions = {"wave", "bosskill"}
 
 
 -- locals
-local timer = {}
-local icon = {}
+local timer = {
+    wave = 180,
+}
+local icon = {
+    wave = "Spell_Holy_PrayerOfHealing",
+}
 local syncName = {}
 
+local wave = nil
 
 ------------------------------
 --      Initialization      --
 ------------------------------
+module:RegisterYellEngage(L["trigger1"])
 
 -- called after module is enabled
 function module:OnEnable()	
 	self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
 	
 	self.warnsets = {}
-	for i=1,9 do 
+	for i=0,8 do 
 		self.warnsets[L["trigger"..i]] = L["warn"..i] 
 	end
+    
+    wave = 0
 end
 
 -- called after module is enabled and after each wipe
@@ -105,6 +116,7 @@ end
 
 -- called after boss is engaged
 function module:OnEngage()
+    wave = 1
 end
 
 -- called after boss is disengaged (wipe(retreat) or victory)
@@ -116,8 +128,32 @@ end
 --      Event Handlers	    --
 ------------------------------
 
+function module:CheckForWipe()    
+    -- ignore wipe check
+end
+
 function module:CHAT_MSG_MONSTER_YELL(msg)
+    --if string.find(msg, L["trigger1"]) then
+        
+    
 	if self.db.profile.wave and msg and self.warnsets[msg] then
 		self:Message(self.warnsets[msg], "Urgent")
+        
+        local i = tonumber(string.sub(L:GetReverseTranslation(self.warnsets[msg]), 5))
+        if i == 0 then
+            i = 1
+        end        
+        wave = i
+        
+        self:RemoveBar(L["warn" .. i])
+        if i < 8 then
+            self:Bar(L["warn" .. i+1], timer.wave, icon.wave)
+        end
 	end
+    
+    if self.db.profile.wave and msg and string.find(msg, L["trigger2_2"]) and wave ~= 2 then
+        wave = 2
+        self:RemoveBar(L["warn2"])
+        self:Bar(L["warn3"], timer.wave - 5, icon.wave) -- kill yell around 5s later
+    end
 end
