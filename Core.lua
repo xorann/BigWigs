4aa8eba631f69a24db1c9d694d372571eb0129bf
xorann <xorann@gmail.com>
2017-06-02 18:06:27 +0200

    release

--- Core.lua
+++ Core.lua
@@ -168,7 +168,7 @@ BigWigs.cmdtable = {type = "group", handler = BigWigs, args = {
 }}
 BigWigs:RegisterChatCommand({"/bw", "/BigWigs"}, BigWigs.cmdtable)
 BigWigs.debugFrame = ChatFrame1
-BigWigs.revision = 20008
+BigWigs.revision = 20009
 
 
 function BigWigs:DebugMessage(msg, module)
