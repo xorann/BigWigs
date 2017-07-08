8ae77d183d2cee529995a75412b32789861b42f2
xorann <xorann@gmail.com>
2017-07-08 10:57:04 +0200

    clean up test plugin

--- Plugins/Test.lua
+++ Plugins/Test.lua
@@ -1,12 +1,12 @@
-
 ------------------------------
---      Are you local?      --
+-- Are you local?      		--
 ------------------------------
 
 local L = AceLibrary("AceLocale-2.2"):new("BigWigsTest")
+local health = nil
 
 ----------------------------
---      Localization      --
+-- Localization      	  --
 ----------------------------
 
 L:RegisterTranslations("enUS", function() return {
@@ -30,8 +30,8 @@ L:RegisterTranslations("enUS", function() return {
 	["Testing Sync"] = true,
 	["Test HP Bar 1"] = true,
 	["Test HP Bar 2"] = true,
-
-} end)
+}
+end)
 
 L:RegisterTranslations("deDE", function() return {
 	-- ["test"] = true,
@@ -52,21 +52,22 @@ L:RegisterTranslations("deDE", function() return {
 	["Sync test"] = "Synchronisations-Test",
 	["Perform a sync test of BigWigs."] = "Sychronisations-Test durchführen.",
 	["Testing Sync"] = "Synchronisation testen",
-} end)
+}
+end)
 
 ----------------------------------
---      Module Declaration      --
+-- Module Declaration      		--
 ----------------------------------
 
 BigWigsTest = BigWigs:NewModule(L["Test"])
-BigWigsTest.revision = 20003
+BigWigsTest.revision = 20011
 
 BigWigsTest.consoleCmd = L["test"]
 BigWigsTest.consoleOptions = {
 	type = "group",
 	name = L["Test"],
 	desc = L["Options for testing."],
-	args   = {
+	args = {
 		[L["local"]] = {
 			type = "execute",
 			name = L["Local test"],
@@ -78,7 +79,7 @@ BigWigsTest.consoleOptions = {
 			name = L["Sync test"],
 			desc = L["Perform a sync test of BigWigs."],
 			func = function() BigWigsTest:TriggerEvent("BigWigs_SyncTest") end,
-			disabled = function() return ( not IsRaidLeader() and not IsRaidOfficer() ) end,
+			disabled = function() return (not IsRaidLeader() and not IsRaidOfficer()) end,
 		},
 	}
 }
@@ -100,63 +101,48 @@ function BigWigsTest:BigWigs_RecvSync(sync, rest, nick)
 	if sync == "TestSync" then
 		self:Message(L["Testing Sync"], "Positive")
 		self:Bar(L["Testing Sync"], 10, "Spell_Frost_FrostShock", true, "Green", "Blue", "Yellow", "Red")
-    elseif sync == "TestNumber" and rest then
-        --SendChat(rest)
-        rest = tonumber(rest)
-        if type(rest) == "number" then
-            --SendChat(rest * 2) 
-        end
+	elseif sync == "TestNumber" and rest then
+		rest = tonumber(rest)
+		if type(rest) == "number" then
+		end
 	end
 end
 
 
 function BigWigsTest:BigWigs_Test()
-    self:Message(L["Testing"], "Attention", true, "Long")
+	self:Message(L["Testing"], "Attention", true, "Long")
 	self:Bar(L["Test Bar 4"], 3, "Spell_Nature_ResistNature", true, "black")
-    self:Bar(L["Test Bar 3"], 5, "Spell_Nature_ResistNature", true, "red")
-    self:Bar(L["Test Bar 2"], 16, "Inv_Hammer_Unique_Sulfuras")
+	self:Bar(L["Test Bar 3"], 5, "Spell_Nature_ResistNature", true, "red")
+	self:Bar(L["Test Bar 2"], 16, "Inv_Hammer_Unique_Sulfuras")
 	self:Bar(L["Test Bar"], 20, "Spell_Nature_ResistNature")
-    self:WarningSign("Inv_Hammer_Unique_Sulfuras", 10)
-    
+	self:WarningSign("Inv_Hammer_Unique_Sulfuras", 10)
+
 	self:DelayedMessage(5, L["OMG Bear!"], "Important", true, "Alert")
 	self:DelayedMessage(10, L["*RAWR*"], "Urgent", true, "Alarm")
 	self:DelayedMessage(20, L["Victory!"], "Bosskill", true, "Victory")
-    
-    self:Sync("TestNumber 5")
-    
-    BigWigs:Proximity()
-    
-    local function deactivate()
-        BigWigs:RemoveProximity()
-    end
+
+	self:Sync("TestNumber 5")
+
+	BigWigs:Proximity()
+
+	local function deactivate()
+		BigWigs:RemoveProximity()
+	end
+
 	--HPBar
-	self:TriggerEvent("BigWigs_StartHPBar", self, L["Test HP Bar 1"], 100)
-    --self:TriggerEvent("BigWigs_SetHPBar", self, L["Test HP Bar 1"], 0)
-    self:TriggerEvent("BigWigs_StartHPBar", self, L["Test HP Bar 2"], 100)
-   -- self:TriggerEvent("BigWigs_SetHPBar", self, L["Test HP Bar 2"], 0)
-    health = 100
-	
+	health = 100
+	self:TriggerEvent("BigWigs_StartHPBar", self, L["Test HP Bar 1"], health)
+	self:TriggerEvent("BigWigs_StartHPBar", self, L["Test HP Bar 2"], health)
+
 	self:ScheduleRepeatingEvent("bwtesthpbarrepeat", self.UpdateTestHPBars, 0.1, self)
 
-    
-    self:ScheduleEvent("BigWigsTestOver", deactivate, 20, self)
-    
-    --self:Sync("BossEngaged "..self:ToString())
-    
-	
-    
-    --self:TriggerEvent("BigWigs_StartCounterBar", self, "CounterBar Test", 10, "Spell_Shadow_Charm")
-    --self:TriggerEvent("BigWigs_StartCounterBar", self, "CounterBar Test2", 30, "Spell_Shadow_Charm", true, "red")
+	self:ScheduleEvent("BigWigsTestOver", deactivate, 20, self)
 end
-
---function BigWigsTest:TestCounter()
---    self:TriggerEvent("BigWigs_SetCounterBar", self, "CounterBar Test", 5, true)
---end
 
 function BigWigsTest:UpdateTestHPBars()
 	if health > 0 then
 		health = health - 1
-		self:TriggerEvent("BigWigs_SetHPBar", self, L["Test HP Bar 1"], 100-health)
-		self:TriggerEvent("BigWigs_SetHPBar", self, L["Test HP Bar 2"], 100-health)
+		self:TriggerEvent("BigWigs_SetHPBar", self, L["Test HP Bar 1"], 100 - health)
+		self:TriggerEvent("BigWigs_SetHPBar", self, L["Test HP Bar 2"], 100 - health)
 	end
 end
