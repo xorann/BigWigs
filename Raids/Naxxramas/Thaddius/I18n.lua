046e0e8f392c626fcf5059ab79ee38c27e9a7aea
xorann <xorann@gmail.com>
2018-03-28 17:45:29 +0200

    removed merge messages

--- Raids/Naxxramas/Thaddius/I18n.lua
+++ Raids/Naxxramas/Thaddius/I18n.lua
@@ -78,12 +78,7 @@ L:RegisterTranslations("enUS", function() return {
 	bar_bossActive = "Boss active",
 	
 	-- misc
-<<<<<<< HEAD
-	misc_positiveCharge = "Spell_ChargePositive",--"Interface\\Icons\\Spell_ChargePositive",--
-	misc_negativeCharge = "Spell_ChargeNegative",--"Interface\\Icons\\Spell_ChargeNegative", --
-=======
 	misc_positiveCharge = "Spell_ChargePositive", -- Interface\\Icons\\Spell_ChargePositive
 	misc_negativeCharge = "Spell_ChargeNegative", -- Interface\\Icons\\Spell_ChargeNegative
->>>>>>> development
 
 } end )
